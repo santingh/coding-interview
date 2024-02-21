f7f5a05855ea1a590c70cc0f3252d80303fdce5e
Santosh Singh <mail.sant.singh@gmail.com>
2024-02-21 10:16:27 +0530

    Update Solution-1.java

--- BFS/Number-of-islands/Solution-1.java
+++ BFS/Number-of-islands/Solution-1.java
@@ -1,8 +1,10 @@
 // Solve using BFS
 
+// Solve using BFS
+
 class Solution {
 
-  public int getIslandsCount(char[][] grid){
+  public int numIslands(char[][] grid){
 
     if (grid == null || grid.length == 0 || grid[0].length == 0){
       return 0;
@@ -18,31 +20,35 @@ class Solution {
           // found a new island - increament count and trace every connected land
           ++islandCount;
           Queue<Integer> neighbours = new LinkedList<>();
-          neighbours.offer(row * colCount + col);
+          neighbours.add(row * colCount + col);
+          grid[row][col] = '0'; // mark visited
           while(!neighbours.isEmpty()){
             int pos = neighbours.remove();
-            int landRow = pos / rowCount; 
+            int landRow = pos / colCount; 
             int landCol = pos % colCount; 
-            grid[landRow][landCol] = 0; // mark visited
             // check neighbours if within grid boundaries and value 1
             // check right
             if (landCol < (colCount - 1) && grid[landRow][landCol + 1] == '1'){
               neighbours.offer(landRow * colCount + (landCol + 1));
+              grid[landRow][landCol + 1] = '0'; // mark visited
             }
 
             // check left - grid[landRow][landCol - 1]
             if (landCol > 0 && grid[landRow][landCol - 1] == '1'){
               neighbours.offer(landRow * colCount + (landCol - 1));
+              grid[landRow][landCol - 1] = '0';
             }
 
             // check up - grid[landRow - 1][landCol]
             if (landRow > 0 && grid[landRow - 1][landCol] == '1'){
-              neighbours.offer((landRow - 1) * colCount + landCol);
+              neighbours.add((landRow - 1) * colCount + landCol);
+              grid[landRow - 1][landCol] = '0';
             }
 
             // check down - grid[landRow + 1][landCol]
             if (landRow < (rowCount - 1) && grid[landRow + 1][landCol] == '1'){
-              neighbours.offer((landRow + 1) * colCount + landCol);
+              neighbours.add((landRow + 1) * colCount + landCol);
+              grid[landRow + 1][landCol] = '0';
             }
             
           }
