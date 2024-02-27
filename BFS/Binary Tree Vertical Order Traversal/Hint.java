b3b587215194ac6f0e8117b864f9d36bfd6f5808
Santosh Singh <mail.sant.singh@gmail.com>
2024-02-27 15:26:25 +0530

    Create Hint.java

---
+++ BFS/Binary Tree Vertical Order Traversal/Hint.java
@@ -0,0 +1,4 @@
+Clearly its BFS or DFS problem. 
+Problem wants us to traverse the nodes by order of level and the column. 
+
+We know BFS always gurantees level ordering so BFS is better approach here. 
